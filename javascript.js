f50a15949e14ff41052d41b20a9a5576aa5a972d
supersonickai <maxwellkaimcginnis@gmail.com>
2025-12-08 12:14:46 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 <script>
-  const slope = document.getElementById('slope");
-                                        slope.addEventListener('Click', Function(window.open('https://google.com', '_blank')) {
-                                                               window.open('https://google.com', '_blank');
-});
+  const slope = document.getElementById('slope');
+
+  slope.addEventListener('click', function () {
+    window.open('https://google.com', '_blank');
+  });
 </script>
