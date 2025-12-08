7c80e717d1bcc1a7a9ab0fa0e208a1731d6bc717
supersonickai <maxwellkaimcginnis@gmail.com>
2025-12-08 14:28:44 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
-
-  const slope = document.getElementById('slope');
 <script>
+  const slope = document.getElementById('slope');
   slope.addEventListener('click', function () {
     window.open('https://google.com', '_blank');
   });
