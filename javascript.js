de235e2ad1fb6b6b91f18a433000da0111a798fc
supersonickai <maxwellkaimcginnis@gmail.com>
2025-12-08 15:23:49 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 <script>
   const slope = document.getElementById('slope');
   slope.addEventListener('click', function () {
-    window.open('https://https://supersonickai.github.io/gooberville/Slope', '_blank');
+    window.open('https://supersonickai.github.io/gooberville/Slope', '_blank');
   });
 </script>
