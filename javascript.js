676344955b944ddf399bd627aa601b21279c2f5e
supersonickai <maxwellkaimcginnis@gmail.com>
2025-12-09 09:43:25 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 <script>
   const slope = document.getElementById('slope');
   slope.addEventListener('click', function () {
-    window.open('https://supersonickai.github.io/gooberville/Slope', '_blank');
+    window.open('https://supersonickai.github.io/gooberville/Games/L2F.html', '_blank');
   });
 </script>
