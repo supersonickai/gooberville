1264b487284142caa1fb0c271ecd1b4895ecbf40
supersonickai <maxwellkaimcginnis@gmail.com>
2025-12-10 10:56:50 -0800

    Update gooberscript.js

--- gooberscript.js
+++ gooberscript.js
@@ -1,4 +1,4 @@
 //Routs start button to main menu
   document.getElementById('start-button').addEventListener('click', () => {
-    window.open('main menu/');
+    window.open('main menu/index.html');
 });
